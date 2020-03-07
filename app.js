9b620c147d120ec33b95066dd9ca4f4292457f9b
thip <thipsavanh@gmail.com>
2020-03-07 04:35:40 -0600

    edited lib js files

--- app.js
+++ app.js
@@ -27,5 +27,63 @@ inquirer.prompt([
         message: "What is office number for team manager?"
     }
 ]);
+
+const promptUser = async () => {
+    const questions = [
+        {
+            type: "list",
+            name: "title",
+            message: "What type of employee would you like to add?",
+            choices: ["Engineer", "Intern"]
+        },
+        {
+            type: "input",
+            name: "name",
+            message: "Enter employee name."
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "Enter employee ID."
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Enter employee email."
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "Enter employee's GitHub username.",
+            validate: function (title) {
+                if (title.length === "Engineer") {
+                    return true;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "school",
+            message: "Enter employee's school.",
+            validate: function (title) {
+                if (title.length === "Intern") {
+                    return true;
+                }
+            }
+        },
+        {
+            type: "confirm",
+            name: "add",
+            message: "Add another employee?",
+            default: false
+        }
+    ]
+};
+
+async function init() {
+    try {
+        const questions = await promptUser ()
+    }
+}
    
  
